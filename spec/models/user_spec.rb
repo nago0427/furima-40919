573e75e101caec65f51ec303aabdf4c1020a7e01
nago0427 <nago0427@gmail.com>
2024-06-01 21:40:50 +0900

    テストコード追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,12 +51,20 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
 
-      it 'パスワードが半角英数字混合でなければ登録できない' do
+      it '英字のみのパスワードでは登録できない' do
         @user.password = 'password'
         @user.password_confirmation = 'password'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
       end
+
+      it '数字のみのパスワードでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+
 
       it '全角文字を含むパスワードでは登録できない' do
         @user.password = 'passwordあ'
@@ -83,6 +91,18 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
+      it 'お名前(全角)が半角文字を含んでいると登録できない' do
+        @user.last_name = 'yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name is invalid')
+      end
+
+      it 'お名前(全角)が半角文字を含んでいると登録できない' do
+        @user.first_name = 'tarou'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      
       it 'お名前カナ(全角)が空では登録できない' do
         @user.last_name_kana = ''
         @user.valid?
