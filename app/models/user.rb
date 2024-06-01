515269a31ecf714214d95a6a5e41d4a618b321f6
nago0427 <nago0427@gmail.com>
2024-06-01 20:05:06 +0900

    バリデーション追加、修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}ー－]+\z/ }
   validates :birth_date, presence: true
