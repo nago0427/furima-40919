e805f7f538655031b5e2e61737b2d86360f8a660
nago0427 <nago0427@gmail.com>
2024-06-03 21:07:30 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,16 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1}
+  validates :condition_id, numericality: { other_than: 1}
+  validates :shipping_fee_id, numericality: { other_than: 1}
+  validates :prefecture_id, numericality: { other_than: 1}
+  validates :shipping_day_id, numericality: { other_than: 1}
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
