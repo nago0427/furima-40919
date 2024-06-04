1f50ad8752042cd9f0881881b73501ebd59666e1
nago0427 <nago0427@gmail.com>
2024-06-04 15:26:26 +0900

    テストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+
 
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1}
-  validates :condition_id, numericality: { other_than: 1}
-  validates :shipping_fee_id, numericality: { other_than: 1}
-  validates :prefecture_id, numericality: { other_than: 1}
-  validates :shipping_day_id, numericality: { other_than: 1}
+  validates :category_id, presence: true, numericality: { other_than: 1}
+  validates :condition_id, presence: true, numericality: { other_than: 1}
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1}
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'テスト商品' }
+    description { 'これはテスト商品です' }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
+    price { 500 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('app/assets/images/furima-footer.png')), filename: 'test_image.png')
+    end
   end
 end
