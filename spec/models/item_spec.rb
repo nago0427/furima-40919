0869c58f7c2fdf6b0a495ce188e71056c2f73a2d
nago0427 <nago0427@gmail.com>
2024-06-04 16:13:50 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -114,6 +114,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it '出品者が空では出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      
     end
   end
 end
