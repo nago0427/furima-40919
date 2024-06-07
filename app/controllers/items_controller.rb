bd0ee0b52682252ddf1390c1ce34e8a4c70791b9
nago0427 <nago0427@gmail.com>
2024-06-07 21:57:12 +0900

    削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update,:destroy]
   before_action :correct_user, only: [:edit, :update,:destroy]
 
